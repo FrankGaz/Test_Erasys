b7815ae998a7d5355a80526b027c1b88c6c666ee
Frank Artacho <frank.artacho@finetwork.es>
2020-11-11 21:01:45 +0100

    get some data from API and fix some styles

--- myapp/src/Components/ProfileList.js
+++ myapp/src/Components/ProfileList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import http from "../api/http.service";
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
@@ -15,17 +15,18 @@ import "../App.css";
 
 const apiProfiles = "/api/search?length=20&sorting=DISTANCE";
 const profilesAPI = http.get(apiProfiles);
-const useStyles = theme => ({
-    root: {
-        maxWidth: 345,
-      },
-  
-      media: {
-        height: 140,
-      },
+const backgroundColor = "#0C4864";
+const useStyles = (theme) => ({
+  root: {
+    maxWidth: 345,
+    minHeight: 330,
+    backgroundColor: backgroundColor,
+  },
+
+  media: {
+    height: 140,
+  },
 });
-
-
 
 class ProfileList extends React.Component {
   state = {
@@ -36,74 +37,73 @@ class ProfileList extends React.Component {
     profilesAPI.then((res) => {
       const profiles = res.data.items;
       this.setState({ profiles });
-      console.log(profiles.name);
+      console.log(profiles);
     });
   }
-
-
+  
   render() {
     const { classes } = this.props;
     const bull = <span className={classes.bullet}>•</span>;
+    
     return (
-    <Grid className="MyGrid" container spacing={3}>
+        
+      <Grid className="MyGrid" container spacing={3}>
         {this.state.profiles.map((profile, index) => (
           //<p key={index}>{profile.name}</p>
-        
-        <Grid key={index} item xs={12} sm={6} md={3} className="profileCard">
-          
-          <Card className={classes.root}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image={avatarImg}
-                title="Profile picture"
-              />
-              <CardContent className="profileCard_header">
-                <CardActions className="profileCard_header-content">
-                  <Typography gutterBottom variant="h5" component="h2">
-                  {profile.personal} {bull} {profile.name}
-                  </Typography>
-                  <TimelineDot style={{ backgroundColor: "#00cc00" }} />
-                </CardActions>
-              </CardContent>
-              <CardContent className="profileCard_location">
-                <CardActions className="profileCard_location-container">
-                  <div className="profileCard_location-content">
-                    <LocationOnIcon style={{ color: "#fff" }} />
-                    <div className="profileCard_location-data">
-                      <Typography
-                        gutterBottom
-                        variant="subtitle2"
-                        component="span"
-                      >
-                        100 m
-                      </Typography>
-                      <span>|</span>
-                      <Typography
-                        gutterBottom
-                        variant="subtitle2"
-                        component="span"
-                      >
-                        Barcelona
-                      </Typography>
+
+          <Grid key={index} item xs={12} sm={6} md={3} className="profileCard">
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={avatarImg}
+                //   title={profile.picture.comment}
+                />
+                <CardContent className="profileCard_header">
+                  <CardActions className="profileCard_header-content">
+                    <Typography gutterBottom variant="h6" component="h2">
+                      {profile.personal} {bull} {profile.name}
+                    </Typography>
+                    <TimelineDot style={{ backgroundColor: "#00cc00" }} />
+                  </CardActions>
+                </CardContent>
+                <CardContent className="profileCard_location">
+                  <CardActions className="profileCard_location-container">
+                    <div className="profileCard_location-content">
+                      <LocationOnIcon style={{ color: "#fff" }} />
+                      <div className="profileCard_location-data">
+                        <Typography
+                          gutterBottom
+                          variant="subtitle2"
+                          component="span"
+                        >
+                          {profile.location.distance}m
+                        </Typography>
+                        <span>|</span>
+                        <Typography
+                          gutterBottom
+                          variant="subtitle2"
+                          component="span"
+                        >
+                          {profile.location.city}
+                        </Typography>
+                      </div>
                     </div>
-                  </div>
-                </CardActions>
-              </CardContent>
-              <CardContent className="profileCard_body">
-                <Typography
-                  className="profileCard_body-head"
-                  variant="body2"
-                  color="inherit"
-                  component="p"
-                >
-                  Lizards are a widespread group of squamate reptiles, with over
-                  6,000 species, ranging across all continents except Antarctica
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
+                  </CardActions>
+                </CardContent>
+                <CardContent className="profileCard_body">
+                  <Typography
+                    className="profileCard_body-head"
+                    variant="body2"
+                    color="inherit"
+                    component="p"
+                  >
+                    {profile.headline}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
         ))}
       </Grid>
     );
